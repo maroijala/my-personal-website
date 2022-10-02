34dc2a6598a2e513b0d08e1da57148c3cba5729e
Chadmin <chadmin@Chadmins-MacBook-Pro.local>
2022-10-02 14:50:28 +0800

    css_@media_875

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const navTog = document.querySelector('.nav_toggle');
 navTog.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
+
+
+const navList = document.querySelector('.nav_items');
+
+navList.addEventListener('click', () => {
+    document.body.classList.toggle('nav-close');
+});
+
